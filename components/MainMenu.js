aa2d3b6b075dc29df28bde25a3141e67747b79bb
NuclearToilet2022 <99990934+NuclearToilet2022@users.noreply.github.com>
2022-02-25 15:51:34 -0500

    Update MainMenu.js

--- components/MainMenu.js
+++ components/MainMenu.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, ImageBackground } from 'react-native';
+
+const image = { uri: "https://photo-voiture.motorlegend.com/hd/dodge-zeo-concept-45328.jpg" };
 
 class MainMenu extends Component {
   render() {
     return (
       <View>
+        <ImageBackgorund source = {image} resizeMode="cover" style={styles.image}>
         <Text style={{textAlign: 'center'}}> Bienvenue dans l'encyclopedie automobile </Text>
-        <Image
+        /*<Image
           source={{
             uri: 'https://photo-voiture.motorlegend.com/hd/dodge-zeo-concept-45328.jpg',
           }}
           style={{ width: 1920, height: 1080 }}
-        />
+        />*/
+        </ImageBackground>
       </View>
     );
   }
