36d55cf0099da293ba77b0cbafadb64d0eb4cc6f
NuclearToilet2022 <99990934+NuclearToilet2022@users.noreply.github.com>
2022-02-25 15:27:42 -0500

    Update MainMenu.js

--- components/MainMenu.js
+++ components/MainMenu.js
@@ -5,7 +5,7 @@ class MainMenu extends Component {
   render() {
     return (
       <View>
-        <Text> Bienvenue dans l'encyclopedie automobile </Text>
+        <Text style={{textAlign: 'center'}}> Bienvenue dans l'encyclopedie automobile </Text>
         <Image
           source={{
             uri: 'https://photo-voiture.motorlegend.com/hd/dodge-zeo-concept-45328.jpg',
