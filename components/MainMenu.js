e6cda1f0043d9b474de5d28bef558621d4b178b7
NuclearToilet2022 <99990934+NuclearToilet2022@users.noreply.github.com>
2022-02-25 14:58:16 -0500

    Update MainMenu.js

--- components/MainMenu.js
+++ components/MainMenu.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 class MainMenu extends Component {
   render() {
     return (
-      <Text> Bienvenue dans l'encyclopedie automobile ! </Text>
-      //<Image>
+      <Text Bienvenue dans l'encyclopedie automobile />
+      <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{ width: 200, height: 200 }}
+        />
     );
   }
 }
