1a445171a6b86f88fa86dafb06fb558d9b5faefc
NuclearToilet2022 <99990934+NuclearToilet2022@users.noreply.github.com>
2022-02-25 14:42:23 -0500

    Update App.js

--- App.js
+++ App.js
@@ -2,18 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MainMenu from './components/MainMenu';
-import { Card } from 'react-native-paper';
 
-//let a = 5 + 2 + "a"
+let a = 5 + 2 + "a"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      //<Text>ceci est {a}</Text>
-      <Card>
-        <MainMenu />
-      </Card>
+      <Text>ceci est {a}</Text>
       <StatusBar style="auto" />
+      <MainMenu />
     </View>
   );
 }
