eb28b4c5ff2bf97bdad89cd1e932e9b16c09d38a
MaXijo264 <98619050+MaXijo264@users.noreply.github.com>
2022-04-08 16:16:46 -0400

    Classe voiture update
    affiche plus de truc, mais seulement les voitures à essence

--- classes/Voiture/Voiture.js
+++ classes/Voiture/Voiture.js
@@ -14,11 +14,42 @@ export default class Voiture{
               <Text>{this.caracteristiques["ShownName"]}</Text>
               <Image
               style = {{
-                  width: 50,
-                  height: 50
+                  width: 400,
+                  height: 250
                 }}
               source={{uri: this.caracteristiques["Photo extérieur"]}}
               />
+              <Image
+              style = {{
+                  width: 200,
+                  height: 125
+                }}
+              source={{uri: this.caracteristiques["Photo intérieur"]}}
+              />
+              <Text>Prix : {this.caracteristiques["Starting Price"]}</Text>
+              <Text>Type de carburant : {this.caracteristiques.Engine["Energy source"]}</Text>
+              <Text>Engine </Text>
+              <View style = {{paddingLeft: 50, alignItems: "flex-start"}}>
+                <Text>Code : {this.caracteristiques.Engine.Code}</Text> 
+                <Text>Displacement : {this.caracteristiques.Engine.Displacement}</Text>
+                <Text>Cylinder layout : {this.caracteristiques.Engine["Cylinder layout"]}</Text>
+                <Text>Aspiration : {this.caracteristiques.Engine.Aspiration}</Text>
+                <Text>Power : {this.caracteristiques.Engine.Power}</Text>
+                <Text>Torque : {this.caracteristiques.Engine.Torque}</Text>
+                
+               
+                
+              </View>
+              <Text>Body type : {this.caracteristiques["Body type"]}</Text>
+              <Text>Top speed : {this.caracteristiques["Top speed"]}</Text>
+              <Text>Weight : {this.caracteristiques["Weight"]}</Text>
+              <Text>Dimensions </Text>
+              <View style = {{paddingLeft: 50, alignItems: "flex-start"}}>
+                <Text>Wheelbase lenght : {this.caracteristiques.Dimensions["Wheelbase lenght"]}</Text>
+                <Text>Height : {this.caracteristiques.Dimensions.Height}</Text>
+                <Text>Lenght : {this.caracteristiques.Dimensions.Lenght}</Text>
+                <Text>Width : {this.caracteristiques.Dimensions.Width}</Text>
+              </View>
             </View>
             
             
